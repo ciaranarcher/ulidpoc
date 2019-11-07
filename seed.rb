08cad61347e3061cd907f2ff7e508db74d4bcbc4
Ciaran Archer <carcher@zendesk.com>
2019-11-07 13:58:34 +0000

    Add version using SQL function.

---
+++ seed.rb
@@ -0,0 +1,20 @@
+require 'mysql2'
+require 'ulid'
+
+db_host = "localhost"
+db_user = "root"
+db_name = 'biz'
+
+client = Mysql2::Client.new(host: db_host, username: db_user, database: db_name)
+
+# We insert some data using MySQL multi-row insert
+puts "record ULID:" + id = ULID.generate
+client.query("INSERT into `widgets` (`id`, `name`, `account_id`) VALUES (ULID_DECODE('#{id}'), 'test', 100)")
+
+# # read it back
+results = client.query("SELECT ULID_ENCODE(id) as id, name, account_id FROM `widgets` WHERE id = ULID_DECODE('#{id}')")
+
+puts "results:"
+results.each do |row|
+  puts row["id"] + "|" + row["name"]
+end
